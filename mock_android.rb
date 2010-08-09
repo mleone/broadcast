8a2760a55c50f9bb9fa8c7b27fcef7a43335153e
Mike Leone <mleone@tokyo.(none)>
2010-08-09 16:48:52 -0400

    Flesh out mock android class a bit.

--- mock_android.rb
+++ mock_android.rb
@@ -1,9 +1,27 @@
 class Android
+  @@id = 1
+
+  # android api call id
+  def get_id
+    result = @@id
+    @@id += 1
+    result
+  end
+
   def getLastKnownLocation
-    {"error"=>nil, "id"=>4, "result"=>{"passive"=>{"provider"=>"gps", "time"=>1280112285800, "longitude"=>-77.3950646833333, "latitude"=>41.8636186666667, "speed"=>0, "accuracy"=>24, "altitude"=>38.7}, "gps"=>{"provider"=>"gps", "time"=>1280112285800, "longitude"=>-77.3950646833333, "latitude"=>41.8636186666667, "speed"=>0, "accuracy"=>24, "altitude"=>38.7}, "network"=>nil}}
+    {"error"=>nil, "id"=> get_id, "result"=>{"passive"=>{"provider"=>"gps", "time"=>1280112285800, "longitude"=>-77.3950646833333, "latitude"=>41.8636186666667, "speed"=>0, "accuracy"=>24, "altitude"=>38.7}, "gps"=>{"provider"=>"gps", "time"=>1280112285800, "longitude"=>-77.3950646833333, "latitude"=>41.8636186666667, "speed"=>0, "accuracy"=>24, "altitude"=>38.7}, "network"=>nil}}
   end
 
   def batteryGetTemperature
-    return "38"
+    {"error" => nil, "id" => get_id, "result" => 330}
   end
+
+  def batteryGetStatus
+    {"error" => nil, "id" => get_id, "result" => 3}
+  end
+
+  def cameraCapturePicture(path)
+    true
+  end
+
 end
