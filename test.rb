e06f5d434a027b99e98e2ed2f080a172c57ba88e
Mike Leone <mleone@tokyo.(none)>
2010-08-13 03:04:08 -0400

    Add a basic test suite using Rack::Test and vanilla Test::Unit.

---
+++ test.rb
@@ -0,0 +1,36 @@
+require File.join(File.dirname(__FILE__), 'environment')
+require 'test/unit'
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
+
+class HelloWorldTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Broadcast
+  end
+
+  def test_it_shows_index_page
+    get '/'
+    assert last_response.ok?
+  end
+
+  def test_get_snapshot
+    get '/snapshot.jpg'
+    assert last_response.ok?
+    assert last_response.body.length > 10000
+  end
+
+  def test_get_update
+    get '/update.jpg'
+    assert last_response.ok?
+    assert last_response.body.length > 10000
+  end
+
+  def test_get_json
+    get 'location.json'
+    assert last_response.ok?
+  end
+
+end
