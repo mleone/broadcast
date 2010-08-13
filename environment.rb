45bb74ab8d85f0ef25cd87cb23c25012c35d80a4
Mike Leone <mleone@tokyo.(none)>
2010-08-13 03:03:18 -0400

    Factor out some environment setup into an environment.rb

---
+++ environment.rb
@@ -0,0 +1,21 @@
+APP_DIR = File.expand_path File.dirname(__FILE__)
+LIB_DIR = File.join APP_DIR, 'lib'
+PUBLIC_DIR = File.join APP_DIR, "public"
+IMAGE_DIR = File.join PUBLIC_DIR, "images"
+VIEW_DIR = File.join APP_DIR, "views"
+SNAPSHOT_DIR = File.join APP_DIR, "snapshots"
+LOCAL_GEM_DIR = File.join(APP_DIR, 'vendor', 'gems')
+
+$LOAD_PATH << APP_DIR
+$LOAD_PATH << LIB_DIR
+
+# Vendorize all gems in vendor/gems.  Any valid gem with a 'lib' directory will 
+# have its contents added to the ruby load path.
+Dir.entries(LOCAL_GEM_DIR).each do |dir| 
+  gem_lib = File.join LOCAL_GEM_DIR, dir, 'lib'
+  $LOAD_PATH << gem_lib if File.exist?(gem_lib)
+end
+
+
+require 'sinatra/base'
+require 'broadcast'
